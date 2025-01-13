8caec3bda79b02db2f0e6bcecb0e0588cecc81ff
Ahmed Samy <as8609377@gmail.com>
2025-01-13 20:34:26 +0200

    Data storage has been added in localStorage

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,19 @@
 let tack = "";
+if (localStorage.getItem("ta") !== null) {
+  tack = localStorage.getItem("ta");
+}
 function addTack() {
   let input = document.getElementById("input-text");
   tack += `
   <li>${input.value} | ${new Date().toLocaleDateString()}</li>
   `;
+  localStorage.setItem("ta", tack);
   input.value = "";
   showTack();
 }
 
 function showTack() {
-  document.getElementById("tacks").innerHTML = tack;
+  document.getElementById("tacks").innerHTML = localStorage.getItem("ta");
 }
 
 document.getElementById("but").onclick = () => {
